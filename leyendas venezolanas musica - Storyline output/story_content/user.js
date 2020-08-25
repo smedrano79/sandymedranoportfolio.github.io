2868cd5cb0ffb6c1c7e2436bbb0ed7cd8cbbeb3a
Sandy Medrano <30882004+smedrano79@users.noreply.github.com>
2020-08-24 20:34:39 -0400

    Leyendas venezilanas con música
    LVCM

---
+++ leyendas venezolanas musica - Storyline output/story_content/user.js
@@ -0,0 +1,53 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5gL6s3niylE":
+        Script1();
+        break;
+      case "6lsEtX3Wkts":
+        Script2();
+        break;
+      case "6YmQ5ilprEj":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  //load the scripts dynamically into the head of the document
+function add_line() {
+ var line = document.createElement("audio");
+ var head=document.getElementsByTagName('body')[0];
+ line.type = "audio/mp3";
+ line.src="";
+ line.id="bgSong" ;
+ line.autoplay = true;
+ line.loop = true;
+ head.appendChild(line);
+}
+//but we only want to add these once!
+if(document.getElementById('bgSong')==null){
+ add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.5;
+}
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+this.Location= player.GetVar("location");
+var audio = document.getElementById('bgSong');
+audio.src= Location + "Ghost_Story_Kevin MacLeod.mp3";
+audio.load();
+audio.play();
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 2;
+}
+
